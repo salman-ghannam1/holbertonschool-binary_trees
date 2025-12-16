98dde728013a1ad05a62888d5feca88cc0ccc07a
salman_ghannam1 <sloome222@gmail.com>
2025-12-16 23:52:05 +0300

    update balanse factor function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,15 +13,11 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL)
-		left_height = -1;
-	else
-		left_height = (int)binary_tree_height(tree->left);
+	left_height = (tree->left == NULL) ? -1 :
+		(int)binary_tree_height(tree->left);
 
-	if (tree->right == NULL)
-		right_height = -1;
-	else
-		right_height = (int)binary_tree_height(tree->right);
+	right_height = (tree->right == NULL) ? -1 :
+		(int)binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
